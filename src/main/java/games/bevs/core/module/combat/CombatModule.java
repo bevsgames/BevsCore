ce142f8db7912ba0a77d32407cb73d6abbef6fed
Heath Logan Campbell <github@heathlogancampbell.com>
2019-08-05 12:54:39 +1200

    fix entityDamageEvent

--- src/main/java/games/bevs/core/module/combat/CombatModule.java
+++ src/main/java/games/bevs/core/module/combat/CombatModule.java
@@ -15,6 +15,7 @@ import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -189,7 +190,7 @@ public class CombatModule extends Module {
 	 * event
 	 * @param e
 	 */
-	@EventHandler
+	@EventHandler(priority = EventPriority.LOWEST)
 	public void onDamage(EntityDamageEvent e)
 	{
 		
