4546bf2ebddf5e85286e99c81186d745623a1065
Heath Logan Campbell <github@heathlogancampbell.com>
2019-08-09 15:29:52 +1200

    add Vampire ability

---
+++ src/main/java/games/bevs/core/module/abilties/abilities/VampireAbility.java
@@ -0,0 +1,41 @@
+package games.bevs.core.module.abilties.abilities;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.inventory.ItemStack;
+
+import games.bevs.core.module.abilties.AbilityInfo;
+import games.bevs.core.module.abilties.types.Ability;
+import games.bevs.core.module.combat.event.CustomDamageEvent;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * This ability will give the killer more health on kill
+ * 
+ * @owner BevsGames
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@AbilityInfo(name="Vampire", author = "Sprock", description = {"Gain extra hearts when you die"})
+public class VampireAbility extends Ability
+{
+	//1 Minecraft Heart = 2 Heart
+	private @Getter @Setter double maxStealHearts = 4;
+	
+	@EventHandler
+	public void onPunch(CustomDamageEvent e)
+	{
+		if(e.isAttackerIsPlayer() && (this.hasAbility(e.getAttackerPlayer())))
+		{
+			if(!e.isVictimIsPlayer()) 
+				return;
+			Player player = e.getAttackerPlayer();
+			
+			if(e.willDie())
+				player.setHealth(player.getHealth() + this.maxStealHearts);
+		}
+	}
+}
--- src/main/java/games/bevs/core/module/antihax/AntiHaxModule.java
+++ src/main/java/games/bevs/core/module/antihax/AntiHaxModule.java
@@ -1,12 +1,34 @@
 package games.bevs.core.module.antihax;
 
 import games.bevs.core.BevsPlugin;
+import games.bevs.core.commons.CC;
 import games.bevs.core.module.ModInfo;
 import games.bevs.core.module.Module;
 import games.bevs.core.module.commandv2.CommandModule;
 
 /**
  * BevsAntiHax
+ * 
+ * This is a lightweight anticheat made for the BevsGames
+ * So that we could make special made checks for popular
+ * Hack Clients. 
+ * 
+ * We don't need to be the best, we just need to get rid
+ * of a good number of hackers
+ * 
+ * We first will focus on blatant hacks
+ * 
+ * Checks
+ * <ul>
+ * 	<li>Fly</li>
+ * 	<li>Speed</li>
+ * 	<li>Timer</li>
+ *  <li>NoFall</li>
+ * </ul>
+ * 
+ * Note: We don't want peopld to think this anti-cheat is
+ * unbeatable or something like that because it encourages 
+ * people to make client specially for it.
  */
 @ModInfo(name = "BAH")
 public class AntiHaxModule extends Module
@@ -21,5 +43,10 @@ public class AntiHaxModule extends Module
 	public void onEnable()
 	{
 		
+	}
+	
+	public String message(String message)
+	{
+		return CC.darkPurple + "= BAH = " + message;
 	}
 }
