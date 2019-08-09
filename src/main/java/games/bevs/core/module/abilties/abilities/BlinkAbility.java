47cd7d905f9a87dc332bb9a7076df57e9261ca8d
Heath Logan Campbell <github@heathlogancampbell.com>
2019-08-09 14:24:21 +1200

    ChatColor to CC

--- src/main/java/games/bevs/core/module/abilties/abilities/BlinkAbility.java
+++ src/main/java/games/bevs/core/module/abilties/abilities/BlinkAbility.java
@@ -14,6 +14,7 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
 import games.bevs.core.commons.ActionType;
+import games.bevs.core.commons.CC;
 import games.bevs.core.commons.Duration.TimeUnit;
 import games.bevs.core.commons.itemstack.ItemStackBuilder;
 import games.bevs.core.commons.utils.PluginUtils;
@@ -31,7 +32,7 @@ import lombok.Setter;
 public class BlinkAbility extends CooldownAbility 
 {
 	// Ability Settings
-	private @Getter @Setter String itemName = ChatColor.WHITE + "Blink";
+	private @Getter @Setter String itemName = CC.white + "Blink";
 	private @Getter @Setter Material itemMaterial = Material.NETHER_STAR;
 	private @Getter @Setter ActionType actionType = ActionType.AIR;
 	private @Getter @Setter int maxUses = 5;
